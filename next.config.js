900f9f027f249a8c99a3aa15b4401de416a05b3d
Yash Gupta <gyash21@gmail.com>
2023-11-20 14:44:09 +0530

    update- new user data fetch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  /* config options here */
+  images:{
+  domains: ['lh3.googleusercontent.com', 'files.stripe.com']
+  }
 }
-
+ 
 module.exports = nextConfig
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import Stripe from "stripe"
+
+const prisma = new PrismaClient()
+
+export const options = {
+  adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+      // Optional space for adding any other provider
+    ],
+   
+
+    events: {
+
+      createUser: async ({user}) => {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+          apiVersion:"2023-10-16"
+        })
+
+        // Create a Stripe Customer
+
+        const customer = await stripe.customers.create({
+          email: user.email || undefined,
+          name: user.name || undefined,
+        })
+
+         //Also update our prisma user with the stripecustomerid
+
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { stripeCustomerId: customer.id },
+      })
+      }
+    }
+    
+
+ 
+  }
+
+  export default NextAuth(options)
