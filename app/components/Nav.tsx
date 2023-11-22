774be8e757a48b64db4d9a8b08fa473792e53ca2
Yash Gupta <gyash21@gmail.com>
2023-11-22 13:13:23 +0530

    update- cart icon setup

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Cart from './Cart'
 import { useCartStore } from '@/store'
+import { AiFillShopping } from 'react-icons/ai'
 
 
 
@@ -13,12 +14,16 @@ import { useCartStore } from '@/store'
 export default function Nav({user}: Session){
     const cartStore = useCartStore()
     return(
-        <nav className='flex justify-between items-center py-8'>
+        <nav className='flex justify-between items-center py-12'>
             <Link href={"/"}>
             <h1 className='text-2xl text-teal-700 font-bold'>Next-Ecom</h1>
             </Link>
             <ul className='flex items-center gap-12'>    
-              {/* If the use is not signed in        */}
+              {/* If the use is not signed in */}
+              <li className='flex items-center text-3xl relative cursor-pointer'>
+                <AiFillShopping/>
+              </li>
+
                 {!user && (
                     <li className='bg-teal-700 text-white py-2 px-4 rounded-md'>
                     <button onClick={() => signIn()}>Sign In</button>
