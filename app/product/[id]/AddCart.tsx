96dd3b319c5528a36da90bf278927ee96234e1ab
Yash Gupta <gyash21@gmail.com>
2023-11-25 23:34:22 +0530

    update- types for AddCart

--- app/product/[id]/AddCart.tsx
+++ app/product/[id]/AddCart.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { useCartStore } from "@/store";
+import { AddCartType } from "@/types/AddCartType";
 
 
-export default function AddCart(){
+export default function AddCart({name, id, image, unit_amount} : AddCartType){
     return(
         <>
-        <button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-800">
+        <button onClick={} className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-800">
             Add to cart
             </button>
         </>
