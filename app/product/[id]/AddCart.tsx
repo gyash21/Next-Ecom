6858008d757ed90db063a8508306c045eb6b1205
Yash Gupta <gyash21@gmail.com>
2023-11-25 23:45:03 +0530

    update- creating Add Product functionality

--- app/product/[id]/AddCart.tsx
+++ app/product/[id]/AddCart.tsx
@@ -4,7 +4,9 @@ import { useCartStore } from "@/store";
 import { AddCartType } from "@/types/AddCartType";
 
 
-export default function AddCart({name, id, image, unit_amount} : AddCartType){
+export default function AddCart({name, id, image, unit_amount, quantity} : AddCartType){
+    const cartStore = useCartStore();
+
     return(
         <>
         <button onClick={} className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-800">
--- store.ts
+++ store.ts
@@ -1,21 +1,15 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware"
+import { AddCartType } from "./types/AddCartType";
 
 
-type CartItem = {
-    name: string,
-    id: string,
-    images?: string[],
-    description?: string,
-    unit_amount: number,
-    quantity: number
-}
+
 
 type CartState = {
-
+    cart: AddCartType[],
     isOpen: boolean,
-    cart: CartItem[],
     toggleCart: () => void
+    addProduct: (item: AddCartType) => void
 
 }
 
@@ -26,6 +20,20 @@ export const useCartStore = create<CartState>()(
             cart: [],
             isOpen: false,
             toggleCart: () => set((state) => ({isOpen: !state.isOpen})),
+            addProduct: (item) => set((state) => {
+                const existingItem = state.cart.find(cartItem => cartItem.id === item.id)
+                if(existingItem){
+                    const updatedCart = state.cart.map((cartItem) => {
+                        if(cartItem.id === item.id){
+                            return{...cartItem, quantity: cartItem.quantity + 1}
+                        }
+                        return cartItem
+                    })
+                    return { cart: updatedCart}
+                }else{
+                    return{ cart: [...state.cart, {...item, quantity:1}]}
+                }
+            })
         }),
 
        
