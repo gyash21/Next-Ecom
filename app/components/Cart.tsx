f52d7d8de3ca75b6d6a5fc0ff633e9d5fe7f692e
Yash Gupta <gyash21@gmail.com>
2023-11-26 23:40:20 +0530

    update- cart ui 1

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -7,11 +7,15 @@ export default function Cart(){
 
     const cartStore = useCartStore()
 
-    cartStore.isOpen
-
     return(
-        <div>
-            <h1>Cart</h1>
+        <div onClick={() => cartStore.toggleCart()} className="fixed w-full h-screen left-0 top-0 bg-black/25">
+
+            <div className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-800">
+                <h1>
+                    Here's your shopping list📃
+                </h1>
+            </div>
+            
         </div>
     )
 }
