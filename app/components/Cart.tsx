ea5e4e7e6e9e92fdf90e391bb19a6d6fb66704c8
Yash Gupta <gyash21@gmail.com>
2023-11-26 23:43:21 +0530

    update- avoid propagation on child element

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -10,7 +10,7 @@ export default function Cart(){
     return(
         <div onClick={() => cartStore.toggleCart()} className="fixed w-full h-screen left-0 top-0 bg-black/25">
 
-            <div className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-800">
+            <div onClick={(e) => e.stopPropagation()} className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-800">
                 <h1>
                     Here's your shopping list📃
                 </h1>
