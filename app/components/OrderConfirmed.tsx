bae06c2f3b75638d0bcff86344e01331244ac25a
Yash Gupta <gyash21@gmail.com>
2023-12-06 23:59:48 +0530

    update- more details

--- app/components/OrderConfirmed.tsx
+++ app/components/OrderConfirmed.tsx
@@ -1,12 +1,18 @@
 'use client'
 
 import { motion } from "framer-motion"
+import Image from "next/image"
 
 
 export default function OrderConfirmed(){
     return(
         <motion.div initial={{scale: 0.5, opacity:0}} animate={{scale: 1, opacity:1}}>
 
+        <div>
+            <h1>Your order has been placed</h1>
+            <h2>Check your email for the receipt</h2>
+            <Image/>
+        </div>
 
         </motion.div>
 
